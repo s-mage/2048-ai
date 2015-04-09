a2f63cbc88d778a4110771e85861c8f1f6056cfb
Sergey Smagin <smaginsergey1310@gmail.com>
2015-04-09 21:18:33 +0300

    Initial commit

---
+++ src/twentyfortyeight_ai/ai.cljs
@@ -0,0 +1,94 @@
+(ns twentyfortyeight-ai.ai
+  (:require [twentyfortyeight-ai.core :as c]
+            [om.core :as om :include-macros true]))
+
+(declare score)
+
+(defn nature-moves [board]
+  (filter #(not (nil? %)) (map-indexed #(if (nil? %2) (assoc board %1 2)) board)))
+
+(def directions [:up :right :down :left])
+
+(defn moves [board]
+  (reduce #(conj %1 {%2 (c/process-move board %2)}) {} directions))
+
+(defn possible-moves [board]
+  (filter #(not= board (val %)) (moves board)))
+
+(def player-moves possible-moves)
+
+(defn one-step-forward [board]
+  "Nature moves and player moves, returns list of boards."
+  (distinct (apply concat (map #(vals (player-moves %)) (nature-moves board)))))
+
+; (defn scored-one-step-forward [board]
+;   "Nature moves and player moves, returns list of boards."
+;   (let [nested-moves (map #(vals (player-moves %)) (nature-moves board))]
+;     (distinct (map #(min (map score %)) nested-moves))))
+
+(defn two-steps-forward [board]
+  (distinct (apply concat (map one-step-forward (one-step-forward board)))))
+
+(defn moves-results [board]
+  (reduce #(conj %1 {(key %2) (score (val %2))}) {} (possible-moves board)))
+
+(defn next-move [board]
+  (key (apply min-key val (moves-results board))))
+
+(defn win? [board]
+  (boolean (some #{2048} [board])))
+
+(defn lose? [board]
+  (empty? (possible-moves board)))
+
+(defn gameover? [board]
+  (or (win? board) (lose? board)))
+
+(defn transpose [board] (apply interleave (partition 4 board)))
+
+(defn one-line-score [v]
+  (reduce + (map #(let [[x y] %] (Math/abs (- x y))) (partition 2 1 v))))
+
+(defn subscore [v]
+  (reduce + (map one-line-score (partition 4 v))))
+
+(defn empty-score [board]
+  (+ 1 (count (filter nil? board))))
+
+(defn max-in-corner-score [board]
+  (let [corners [0 3 12 15]
+        el (apply max board)
+        els-in-corners (map board corners)]
+    (if (some #{el} els-in-corners)
+      1.5
+      1)))
+
+(defn lose-score [board]
+  (if (lose? board) 0.01 1))
+
+(defn score [board]
+  (/ (+ (subscore board) (subscore (transpose board)))
+     (empty-score board)
+     (max-in-corner-score board)
+     (lose-score board)))
+
+(defn board->score [board]
+  "Get min score for direction in minimax procedure"
+  (let [moves (one-step-forward board)
+        scores (map score moves)]
+    (apply min scores)))
+
+(defn minimax [board]
+  "User move, then nature move, then user move again.
+  Choose direction on which final score will be min"
+  (let [score-fun (if (< (apply max board) 128) score #(+ (board->score %) (score %)))
+        nmr (reduce #(conj %1 {(key %2) (score-fun (val %2))}) {} (player-moves board))]
+    (key (apply min-key val nmr))))
+
+(defn move! []
+  (c/move (minimax @c/board-state) (om/root-cursor c/board-state)))
+
+
+(defn play! []
+  (while (not (gameover? @c/board-state))
+    (move!)))
---
+++ src/twentyfortyeight_ai/core.cljs
@@ -0,0 +1,250 @@
+(ns ^:figwheel-always twentyfortyeight-ai.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]
+            [dommy.core :as dommy]
+            [twentyfortyeight-ai.ai :as ai]))
+
+(enable-console-print!)
+
+(def not-nil? (complement nil?))
+
+(defn randomize-start []
+  "Returns two different integers from 0 to 15.  These are the two
+  initial positions that start with 2."
+  (let [x (rand-int 16)
+        y (rand-int 16)]
+    (if (= x y)
+      (recur)
+      [x y])))
+
+(defn init-board []
+  "Creates a 2048 game board.
+  A board is a vector containing 16 elements representative
+  of three rows.  14 of the elements will begin as 0.  2 will
+  have the value of 2."
+  (let [[x y] (randomize-start)
+        coll (vec (repeat 16 nil))]
+    (assoc coll x 2 y 2)))
+
+(def board-state (atom (init-board)))
+(def message-state (atom {:msg nil}))
+
+(def board-edges {:left [0 4 8 12]
+                  :right [3 7 11 15]
+                  :top [0 1 2 3]
+                  :bottom [12 13 14 15]})
+
+(def keycodes {37 :left ;; keycodes with vi mode!
+               38 :up
+               39 :right
+               40 :down
+               72 :left
+               75 :up
+               76 :right
+               74 :down})
+
+(def key-ch (chan))
+
+(defn evt->dir [e]
+  "Translates a key code to a potential direction keyword."
+  (keycodes (.-which e)))
+
+(defn key-listener [ch]
+  "When a directional key has been pressed, we drop it into
+  a channel so it can be processed by something else."
+  (dommy/listen! js/window
+                 :keyup
+                 #(when-let [k (evt->dir %)]
+                    (put! ch k)
+                    (.preventDefault %))))
+
+(dommy/listen! (. js/document (getElementById "move"))
+                 :click
+                 ai/move!)
+
+(dommy/listen! (. js/document (getElementById "moves"))
+                 :click
+                 #(dotimes [n 20] (ai/move!)))
+
+(key-listener key-ch)
+
+(defn get-open [coll]
+  "Returns the indices of all zero items in a collection."
+  (map first (filter #(nil? (second %)) (map-indexed vector coll))))
+
+(def num->score (reduce #(let [x (Math/pow 2 (+ %2 1))] (conj %1 {x (* x %2)})) {} (range 11)))
+
+(defn score [atom-board]
+  (reduce + (map num->score (filter not-nil? @atom-board))))
+
+(defn choose-random [coll]
+  "Given a vector, chooses a random element."
+  (get coll (rand-int (count coll))))
+
+(defn add-new-block [coll]
+  "Adds a new block to a random location on the board."
+  (if-let [open ((comp choose-random vec get-open) coll)]
+    (assoc coll open 2)
+    coll))
+
+(defn movable? [direction coll]
+  "Given a direction and collection representing the board's current state,
+  determines if a move in that direction is possible."
+  (let [edge (direction board-edges)
+        xs (map #(nth coll %) edge)]
+    (boolean (some nil? xs))))
+
+(defn pad-with-nils [coll]
+  "A helper function to ensure coll has 4 elements.  Pads with
+  nils if less than 4."
+  (take 4 (concat coll (repeat nil))))
+
+(defn combine [xs ys]
+  "Given two collections of non-nil elements,
+  recursively combines them according to the game rules.
+
+  Example:
+  [] [2 2 4 8]
+
+  ;; => [4 4 8]
+  "
+  (let [x (first ys)
+        y (second ys)]
+    (if (seq ys)
+      (if (= x y)
+        (combine (conj xs (+ x y)) (drop 2 ys))
+        (combine (conj xs x) (drop 1 ys)))
+      xs)))
+
+(defn combine-row [coll]
+  "A helper function to take a collection representing a
+  game row, filter out nil elements, combine the tiles where
+  appropriate, and pad it back up with nils."
+  (let [filtered (filter not-nil? coll)
+        combined (combine [] filtered)]
+    (pad-with-nils combined)))
+
+(defn combine-rows [coll-of-colls]
+  (map combine-row coll-of-colls))
+
+(defn rearrange-rows [coll-of-colls]
+  "Creates a new collection with all nil elements at the end of each coll"
+  (let [not-nils (map #(filter not-nil? %) coll-of-colls)]
+    (map pad-with-nils not-nils)))
+
+(defn position-rows [coll vertical]
+  "Partitions the rows in the correct orientation depending upon
+  the direction."
+  (if vertical
+    (->> coll
+         (partition 4)
+         (apply interleave)
+         (partition 4))
+    (partition 4 coll)))
+
+(defn vertical-rows? [direction]
+  "Pred for determining if we need to partition into columns."
+  (or (= direction :up) (= direction :down)))
+
+(defn slide? [direction]
+  "Pred for determining if we need to put the nils before the
+  non-nils."
+  (or (= direction :right) (= direction :down)))
+
+(defn slide [coll]
+  (let [not-nils (filter not-nil? coll)
+        nils (repeat (- 4 (count not-nils)) nil)]
+    (concat nils not-nils)))
+
+(defn arrange-and-combine [coll direction]
+  (let [should-slide (slide? direction)
+        colls (-> coll
+                  (position-rows (vertical-rows? direction))
+                  rearrange-rows
+                  combine-rows)]
+    (if should-slide
+      (map slide colls)
+      colls)))
+
+(defn process-move [coll direction]
+  "Given a direction, reorients and combines the tiles according
+  to game rules.  If applicable, we add a new block."
+  (let [colls (arrange-and-combine coll direction)]
+    (if (vertical-rows? direction)
+      (vec (flatten (apply interleave colls)))
+      (vec (flatten colls)))))
+
+(def loser? ai/lose?)
+
+(def winner? ai/win?)
+
+(defn move [direction cursor]
+  "Updates the application state when a move has been issued."
+  (let [data @cursor
+        moved (process-move data direction)]
+    (when-not (= moved data)
+      (om/update! cursor (add-new-block moved)))))
+
+(defn render-row [coll]
+  "Creates an Om element representing a row.  A row contains 4 blocks."
+  (apply dom/div #js {:className "row"} coll))
+
+(defn render-block [x]
+  "Creates an Om element representing a block within a row.  There are 16
+  total blocks in the game."
+  (dom/div #js {:className "block"}
+           (dom/h2 #js {:className "number"} x)))
+
+(defn message [app owner]
+  "An Om component representing a message, either Winner or Game Over."
+  (reify
+    om/IRender
+    (render [_]
+      (dom/h1 #js {:display (if (:msg app)
+                              "block"
+                              "none")} (:msg app)))))
+
+(defn score-board [app owner]
+  "An Om component representing the score board."
+  (reify
+    om/IRender
+    (render [this]
+      (dom/h1 nil (str "Score: " (score app))))))
+
+(defn game-board [app owner]
+  "An Om component representing the game board."
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (let [keypresses key-ch]
+        (go (loop []
+              (let [direction (<! keypresses)]
+                (move direction app)
+                (recur))))))
+    om/IWillUpdate
+    (will-update [this next-state next-props]
+      (when (winner? next-state) (swap! message-state assoc :msg "Winner!"))
+      (when (loser? next-state) (swap! message-state assoc :msg "Loser!")))
+    om/IRender
+    (render [this]
+      (let [rows (partition 4 (map render-block app))]
+        (apply dom/div
+               nil
+               (map #(render-row %) rows))))))
+
+; Attach to DOM
+;
+
+(om/root score-board
+         board-state
+         {:target (. js/document (getElementById "score"))})
+
+(om/root message
+         message-state
+         {:target (. js/document (getElementById "message"))})
+
+(om/root game-board
+         board-state
+         {:target (. js/document (getElementById "game"))})
